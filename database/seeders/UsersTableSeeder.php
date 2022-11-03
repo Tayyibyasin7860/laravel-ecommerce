5f3232fe9b72affd77c216cf2318117470da2d66
Tayyab Yasin <green.saleem@yahoo.com>
2022-11-03 20:20:58 +0500

    fix users table seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class UsersTableSeeder extends Seeder
 {
@@ -15,6 +17,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(5)->create();
+        if(!User::where('email','customer@example.com')->exists()) {
+            User::create([
+                'name' => 'Customer',
+                'email' => 'customer@example.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+            ]);
+        }
+
+        User::factory(4)->create();
     }
 }
